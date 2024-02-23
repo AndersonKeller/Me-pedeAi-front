64ee0ad8d46761843c35b9d5f3bc1741bc491d20
AndersonKeller <andersonkellervendas@hotmail.com>
2024-02-22 23:30:50 -0300

    feat: menu layout improvments and listing infos

--- src/app/cardapio/page.tsx
+++ src/app/cardapio/page.tsx
@@ -9,6 +9,7 @@ export default function Cardapio() {
   const api = new Service();
   async function getMenu() {
     const res = await api.getMenu();
+    console.log(res)
     if (res) {
       setMenu(res);
     }
@@ -20,11 +21,25 @@ export default function Cardapio() {
   return (
     <>
       <Menu />
-      <ul className="w-full bg-gray-800 p-4 flex flex-wrap gap-4">
+      <ul className="w-full h-[calc(100vh-80px)] overflow-auto bg-gray-800 p-4">
+        <div className="flex flex-wrap justify-start gap-3">
         {menu.id &&
           menu.product.map((item) => {
-            return <li key={item.id}>{item.name}</li>;
+            return <li className="relative overflow-hidden w-[19%] min-w-[180px] p-2 rounded-lg shadow-md shadow-gray-400 cursor-pointer hover:scale-105 transition-all bg-gray-400" key={item.id}>
+              <p>{item.type.name}</p>
+              <p>{item.name}</p>
+              <p>{item.description}</p>
+              <p>R$ {item.price.toFixed(2)}</p>
+              <div className="flex justify-between w-[15%] flex-col bg-gray-100 absolute right-0 top-0 h-full">
+                <button className="text-gray-950">1</button>
+                <button className="text-gray-950">2</button>
+                <button className="text-gray-950">3</button>
+                <button className="text-gray-950">4</button>
+              </div>
+            </li>
+           
           })}
+          </div>
       </ul>
     </>
   );
--- src/components/order/Card.tsx
+++ src/components/order/Card.tsx
@@ -27,16 +27,20 @@ export function CardOrder(){
     useEffect(()=>{
       console.log("att")
     },[orders])
-    return <ul className="flex flex-wrap gap-2 items-center justify-start px-2">{orders.map((item)=>{
-        return <li className="rounded-lg bg-gray-300 p-2" key={item.id}>
+    return <ul className="w-full h-[calc(100vh-80px)] overflow-auto bg-gray-800 p-4">
+      <div className="flex flex-wrap justify-start gap-3">
+      {orders.map((item)=>{
+        
+        return <li className="w-[16%] min-w-[180px] rounded-lg bg-gray-300 p-2 shadow-md shadow-gray-400 cursor-pointer hover:scale-105 transition-all" key={item.id}>
           <p className="text-sm text-gray-900">{item.id} - {item.client.name}</p>
           <p className="text-sm text-gray-900 text-right font-bold">{item.client.phone}</p>
           <p className={`${item.order_type==='delivery'?'bg-green-800':'bg-gray-700'} text-sm rounded-lg p-1`}>{item.order_type}</p>
           <p className="text-sm text-blue-900 font-bold text-right">R$ {item.total.toFixed(2)}</p>
-          <button onClick={()=>openOrder(item)} className="bg-gray-200 px-2 py-1 rounded-lg w-full text-sm border border-red-800 text-gray-900 uppercase font-semibold" >Ver</button>
+          <button onClick={()=>openOrder(item)} className="bg-gray-200 px-2 py-1 rounded-lg text-sm border border-red-800 text-gray-900 hover:scale-105 transition-all uppercase font-semibold" >Ver</button>
           </li>
-          
+         
         })}
     {openDetail&&<Orderdetail  order={detailOrder}/>}
+    </div>
     </ul>
 }
